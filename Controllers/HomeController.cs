182eda2b91406ff67c48157fb6eb4ccad4259d95
xiongweichan <xiongweichan@gmail.com>
2019-08-11 21:24:38 +0800

    123
    1111

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
+
+namespace StudentManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IStudentRepository _studentRepository;
+
+        public HomeController(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+        //操作方法
+        public string Index()
+        {
+            return _studentRepository.GetStudent(1).Name;
+            //return Json(new { id = "1", name = "张三" });
+        }
+        //public JsonResult Details()
+        //{
+        //    return Json(_studentRepository.GetStudent(1));
+        //    //return Json(new { id = "1", name = "张三" });
+        //}
+        //内容协商，根据请求来返回格式，要什么格式给什么格式
+        //public ObjectResult Details()
+        //{
+        //    return new ObjectResult(_studentRepository.GetStudent(1));
+        //}
+        public IActionResult Details()
+        {
+            var model = _studentRepository.GetStudent(1);
+
+            //ViewBag.PageTitle = "学生详情";
+            //ViewBag.Student = model;
+            //ViewData["PageTitle"] = "Student Details";
+            //ViewData["Student"] = model;
+
+            return View(model);
+        }
+    }
+}
---
+++ Models/MockStudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    public class MockStudentRepository : IStudentRepository
+    {
+        List<Student> _studentList;
+
+        public MockStudentRepository()
+        {
+            _studentList = new List<Student>
+            {
+                new Student{Id=1,Name="张三",ClassName="一年级",Email="Tony-zhang@52abp.com"},
+                new Student{Id=2,Name="李四",ClassName="二年级",Email="lisi@52abp.com"},
+                new Student{Id=3,Name="王二麻子",ClassName="三年级",Email="wang@52abp.com"},
+            };
+        }
+        public Student GetStudent(int id)
+        {
+            return _studentList.FirstOrDefault(t => t.Id == id);
+        }
+    }
+}
